ff8558109eb97f02437935a71738c3b0d3491e60
Dan Neumann, Jared Friese, Nick Mahoney, Pouya Karimi and Yusef Asjana <pair+dneumann+jfriese+nmahoney+pkarimi+yasjana@pivotal.io>
2015-08-13 08:55:57 -0700

    [#101133750] clone project and start first Integration test

---
+++ src/test/java/io/pivotal/pulse/ProjectControllerIntegrationTest.java
@@ -0,0 +1,36 @@
+package io.pivotal.pulse;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CleanPulseApplication.class)
+@WebAppConfiguration
+public class ProjectControllerIntegrationTest {
+
+    @Autowired private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void create_whenCallWithValidParameter_createsNewProject() throws Exception {
+        mockMvc.perform(post("/project"))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
+}
